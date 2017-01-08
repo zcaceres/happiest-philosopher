c2231b1703f519d1ed93dad353466f3535cfa796
zc <zachcaceres@gmail.com>
2017-01-07 22:09:34 -0500

    Readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# THE HAPPIEST PHILOSOPHER
+### Instant Linguistic Sentiment Analyses and Graph Generator
+#### A Pythonathon 2016 Project by Zach Caceres
+The Happiest Philosopher does sentiment analyses on the text of any .txt file held in the 'philosophers' directory.
+
+It analyzes the POLARITY and the SUBJECTIVITY of any text and generates a graph 
+ranking the authors accordingly.
+
+To use:
+1) Create a folder in 'philosophers' directory with the name of each AUTHOR you would
+like to analyze.
+
+2) Add any .txt files that belongs to that author to the directory. The name of the .txt file is used in
+your results graph, so name accordingly!
+
+3) Run main.py. You need a plotly account which will automatically generate a 
+graph and open your browser to show it to you at the end of the analyses.
--- pyramid_happiestphilosopher/services/main.py
+++ pyramid_happiestphilosopher/services/main.py
@@ -4,29 +4,20 @@ from graphs import convert_data_for_plotting, format_x_val_names
 
 """
 ––––––––––––––––––––– THE HAPPIEST PHILOSOPHER, Instant Linguistic Sentiment Analyses and Graph Generator –––––––––––––
-            Does sentiment analyses on the text of any .txt file held in the 'philosophers' directory.
-
-            Shows you the POLARITY and the SUBJECTIVITY of any text.
-
-            To use:
-            1) Create a folder in 'philosophers' with the name of the AUTHOR.
-
-            2) Add any .txt file that belongs to that author. The name of the .txt file is used in
-            your results graph, so name accordingly!
-
-            3) run main.py. You need a plotly account which will automatically generate a graph and
-            open your browser to show it to you at the end of the analyses.
-––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+                                            See README.md for more!
+                                ------------––––––––––––––––––––––––––––––––––––
 """
+
 
 # Uses TextBlob library to convert text to a TextBlob object, allowing for sentiment analyses
 def convert_to_text_blobs(files_to_analyze):
     text_blobs = []
     for file in files_to_analyze:
         with open(file, 'r') as text_in:
-            blob = TextBlob (text_in.read().replace('\n', ' '))
+            blob = TextBlob(text_in.read().replace('\n', ' '))
             text_blobs.append(blob)
     return text_blobs
+
 
 # Calculates polarity and subjectivity scores
 def sentiment_analysis(author_names, work_names, text_blobs):
@@ -40,6 +31,7 @@ def sentiment_analysis(author_names, work_names, text_blobs):
     return polarity_sentiments, subjectivity_sentiments
 
 
+# Main Method
 if __name__ == '__main__':
     directories_list = prepare_directories_for_processing(PATH_TO_PHILOSOPHERS)
     files_to_analyze, work_names, author_names = get_all_files_for_processing(directories_list)
